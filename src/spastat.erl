ca667773e0b66a036d882f74408ea59685a56db2
Arkadiy Drovosekov <arkdro@gmail.com>
2013-05-09 20:08:22 +0400

    another renaming

--- src/spastat.erl
+++ src/spastat.erl
@@ -1,11 +1,11 @@
 -module(spastat).
 
-%% API for stat_srv
+%% API for spastat_srv
 -export([
          gauge/2
         ]).
 
--define(SERVER, stat_srv).
+-define(SERVER, spastat_srv).
 
 gauge(K, V) ->
     gen_server:cast(?SERVER, {gauge, K, V}).
