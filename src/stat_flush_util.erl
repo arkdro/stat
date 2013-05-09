41edbfd55f1062f2b414d3c1728b43ae5fefc808
Arkadiy Drovosekov <arkdro@gmail.com>
2013-05-09 19:21:11 +0400

    add some unit tests

--- src/stat_flush_util.erl
+++ src/stat_flush_util.erl
@@ -13,6 +13,10 @@
          flush_tab/2
         ]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -define(PERCENTILES, [25, 50, 75, 90, 98]).
 
 -record(acc,{
@@ -101,4 +105,75 @@ get_data(Tab) ->
     Sorted = lists:sort(L),
     Size = ets:info(Tab, size),
     {Size, Sorted}.
+
+%%%===================================================================
+%%% Unit tests
+%%%===================================================================
+
+-ifdef(TEST).
+
+util_test_() ->
+    [{"Percentiles tests",
+      [
+       ?_test(
+          begin
+              Act = get_percentile_indices(20),
+              ?assertMatch({[25,50,75,90,98], [6,11,16,19,20]}, Act)
+          end),
+       ?_test(
+          begin
+              ?assertMatch(1, get_min([1,2,5,61]))
+          end),
+       ?_test(
+          begin
+              ?assertMatch({{[], []}, stub}, fill_percentile(1, 2, {[], []}, stub))
+          end),
+       ?_test(
+          begin
+              {{[15], [425]}, Act} = fill_percentile(425, v2, {[15], [425]}, dict:new()),
+              ActL = dict:to_list(Act),
+              ?assertMatch([{15, v2}], ActL)
+          end),
+       ?_test(
+          begin
+              {{[50], [10]}, Act} = fill_percentile(6, v2, {[25, 50], [5, 10]}, dict:new()),
+              ActL = dict:to_list(Act),
+              ?assertMatch([{50, v2}], ActL)
+          end),
+       ?_test(
+          begin
+              {{[],[]}, Act} = fill_percentile(6, v2, {[50],[5]}, dict:new()),
+              ActL = dict:to_list(Act),
+              ?assertMatch([], ActL)
+          end),
+       ?_test(
+          begin
+              L = [{25,19}, {50, 34}, {75, 49}, {90, 58}, {98, 61}],
+              Act = move_percentiles(dict:from_list(L), #acc{}),
+              Exp = #acc{size=0, perc=lists:sort(L)},
+              ?assertMatch(Exp, Act)
+          end)
+      ]},
+     {"Collect numbers tests",
+      [
+       ?_test(
+          begin
+              Size = 20,
+              Sorted = [X*3+1 || X <- lists:seq(1, Size)],
+              %% [4,7,10,13,16,19, 22,25,28,31,34, 37,40,43,46,49, 52,55,58, 61]
+              Res = collect_numbers({20, Sorted}),
+              Exp = #acc{
+                size = 20,
+                sum = 650,
+                min = 4,
+                max = 61,
+                avg = 32.5,
+                perc = [{25, 19}, {50, 34}, {75, 49}, {90, 58}, {98, 61}]
+                },
+              ?assertMatch(Exp, Res)
+          end)
+      ]}
+    ].
+
+-endif.
 
